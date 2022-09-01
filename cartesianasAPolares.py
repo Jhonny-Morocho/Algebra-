1d20f65f5cbd38215f3c5597a37f850d980aef19
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-08-31 20:25:46 -0500

    2 parte y 3 unido

--- cartesianasAPolares.py
+++ cartesianasAPolares.py
@@ -1,29 +1,29 @@
 import math as math
 import parte2
-
+import parte3
 class Coordenadas:
+
     def menuCoordenadas():
         print("Sistema de Coordenadas Cartesianas.")
         print("Vector P(x,y,z).")
         print()
         try:
-            x = float(input("Ingrese la coordenada x del vector P:"))
-            y = float(input("Ingrese la coordenada y del vector P:"))
-            z = float(input("Ingrese la coordenada z del vector P:"))
+            x = float(input("1. Ingrese la coordenada x del vector P:"))
+            y = float(input("2. Ingrese la coordenada y del vector P:"))
+            z = float(input("3. Ingrese la coordenada z del vector P:"))
             ############# PRIMERO PARTE #################
             ############# PRIMERO PARTE #################
             ############# PRIMERO PARTE #################
-            print("Las coordenadas cartesiandas del vector P son: (", x, ",", y, ",", z, ").")
+            print("LAS COORDENADAS INGRESADAS SON LAS SIGUIENTES DEL VECTOR P SON: (", x, ",", y, ",", z, ").")
             P_modulo = math.sqrt((x**2)+(y**2)+(z**2))
-            
-            
+            print("##########################################")
             print("EL MODULO DEL VECTOR P(x,y,z) ES : ", P_modulo)
             
-            
             ############# SEUNDO PARTE #################
             ############# SEUNDO PARTE #################
             ############# SEUNDO PARTE #################
-            print( "LAS COORDENDAS ESFERICAS SON: ")
+            print("##########################################")
+            print( "LAS COORDENDAS ESFERICAS A CARTECIANAS SON : ")
             #enviar valores para la conversion de cartesians a esfericas
             parte2.Cartesianas.convertirEsfericas(x,y,z)
             
@@ -31,8 +31,11 @@ class Coordenadas:
             ############# TERCERA PARTE #################
             ############# TERCERA PARTE #################
             ############# TERCERA PARTE #################
+            print("##########################################")
+            print( "COORDENADAS ESFERICAS A CARTESIANAS : ")
+            parte3.EsfericasCartesianas.convertirEsfericasCartesianas(x,y,z)
         except:
             print("Ha ocurrido una expecion")
-        
+       
 
 
--- parte2.py
+++ parte2.py
@@ -2,7 +2,6 @@
 import math as math
 class Cartesianas:
     def convertirEsfericas(x,y,z):
-
         print("Las coordenadas cartesiandas del vector P son: (", x, ",", y, ",", z, ").")
         # Sistema de Coordenadas esféricas
         r = math.sqrt((x**2) + (y**2) + (z**2))
@@ -12,10 +11,3 @@ class Cartesianas:
         #fi = math.atan2(y,x)
 
         print("Las coordenadas esféricas del vector P son: (", r, ",", theta, ",", fi, ").")
-        print()
-
-        # sqrt() => Return the square root.
-        P_modulo = math.sqrt((r**2) + (theta**2) + (fi**2))
-        #P_modulo = math.hypot(r,theta,fi)
-        print("El módulo del vector P(r,theta,fi) es: ", P_modulo)
-        print()
